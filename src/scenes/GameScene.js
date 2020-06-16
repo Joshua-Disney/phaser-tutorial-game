2505386d76849906f7fa0195685a82e70f170e72
Mr. Disney <joshua.a.disney@gmail.com>
2020-06-16 12:55:02 -0700

    Add bombs

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,12 @@
 import Phaser from "phaser";
 
+import ScoreLabel from "../ui/ScoreLabel";
+import BombSpawner from "./BombSpawner";
+
 const GROUND_KEY = "ground";
 const DUDE_KEY = "dude";
+const STAR_KEY = "star";
+const BOMB_KEY = "bomb";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -9,13 +14,16 @@ export default class GameScene extends Phaser.Scene {
 
     this.player = undefined;
     this.cursors = undefined;
+    this.scoreLabel = undefined;
+    this.stars = undefined;
+    this.bombSpawner = undefined;
   }
 
   preload() {
     this.load.image("sky", "assets/sky.png");
     this.load.image(GROUND_KEY, "assets/platform.png");
-    this.load.image("star", "assets/star.png");
-    this.load.image("bomb", "assets/bomb.png");
+    this.load.image(STAR_KEY, "assets/star.png");
+    this.load.image(BOMB_KEY, "assets/bomb.png");
 
     this.load.spritesheet(DUDE_KEY, "assets/dude.png", {
       frameWidth: 32,
@@ -28,8 +36,24 @@ export default class GameScene extends Phaser.Scene {
 
     const platforms = this.createPlatforms();
     this.player = this.createPlayer();
+    this.stars = this.createStars();
+
+    this.scoreLabel = this.createScoreLabel(16, 16, 0);
+
+    this.bombSpawner = new BombSpawner(this, BOMB_KEY);
+    const bombsGroup = this.bombSpawner.group;
 
     this.physics.add.collider(this.player, platforms);
+    this.physics.add.collider(this.stars, platforms);
+    this.physics.add.collider(bombsGroup, platforms);
+
+    this.physics.add.overlap(
+      this.player,
+      this.stars,
+      this.collectStar,
+      null,
+      this
+    );
 
     this.cursors = this.input.keyboard.createCursorKeys();
   }
@@ -52,6 +76,30 @@ export default class GameScene extends Phaser.Scene {
     if (this.cursors.up.isDown && this.player.body.touching.down) {
       this.player.setVelocityY(-330);
     }
+  }
+
+  collectStar(player, star) {
+    star.disableBody(true, true);
+
+    this.scoreLabel.add(10);
+
+    if (this.stars.countActive(true) === 0) {
+      //  A new batch of stars to collect
+      this.stars.children.iterate((child) => {
+        child.enableBody(true, child.x, 0, true, true);
+      });
+    }
+
+    this.bombSpawner.spawn(player.x);
+  }
+
+  createScoreLabel(x, y, score) {
+    const style = { fontSize: "32px", fill: "#000" };
+    const label = new ScoreLabel(this, x, y, score, style);
+
+    this.add.existing(label);
+
+    return label;
   }
 
   createPlatforms() {
@@ -92,5 +140,19 @@ export default class GameScene extends Phaser.Scene {
     });
 
     return player;
+  }
+
+  createStars() {
+    const stars = this.physics.add.group({
+      key: STAR_KEY,
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 },
+    });
+
+    stars.children.iterate((child) => {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    return stars;
   }
 }
